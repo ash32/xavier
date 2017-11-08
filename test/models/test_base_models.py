0316fe61beeed467a8db21d878f0b6dfe7f9c43d
Ashwin Kamath <ashwin@lvl6.com>
2017-11-07 23:42:05 -0800

    finished buy_and_hold test

--- test/models/test_base_models.py
+++ test/models/test_base_models.py
@@ -1,30 +1,36 @@
+import os
+
 import numpy as np
 import pytest
 
+from xavier.helpers import normalize
 from xavier.models.buy_and_hold import BuyAndHoldModel
 from xavier.models.const_distribution import ConstantDistributionModel
 
 
 @pytest.fixture
-def closing_price_rel_vec():
-    import os
-    print(os.getcwd())
-    assert os.getcwd() == 'abc'
-    return np.load('../test_data/closing_prices_rel_vec.npy')
+def closing_price_rel_vec(request):
+    return np.load(os.path.join(request.fspath.dirname, 'data', 'closing_prices_rel_vec.npy'))
 
 
 def test_const_dist_model(closing_price_rel_vec):
     model = ConstantDistributionModel()
 
     periods, num_assets = closing_price_rel_vec.shape
-    expected_weights = np.ones(num_assets+1) / (num_assets+1)
+    expected_weights = np.ones(num_assets) / num_assets
     for i in range(periods):
-        weights = model.get_distribution_weights(closing_price_rel_vec[i], None)
-        assert weights == expected_weights
+        weights = model.get_distribution_weights(closing_price_rel_vec[i], expected_weights)
+        np.testing.assert_equal(weights, expected_weights)
 
 
 def test_buy_and_hold(closing_price_rel_vec):
     model = BuyAndHoldModel()
 
-    num_assets = closing_price_rel_vec.shape[0]
-    initial_weights = np.ones(num_assets+1) / (num_assets+1)
+    periods, num_assets = closing_price_rel_vec.shape
+    prev_weights = np.ones(num_assets) / num_assets
+    expected_weights = normalize(np.cumprod(closing_price_rel_vec, axis=0), axis=1)
+    for i in range(periods):
+        weights = model.get_distribution_weights(closing_price_rel_vec[i], prev_weights)
+        np.testing.assert_allclose(weights, expected_weights[i])
+
+        prev_weights = weights
